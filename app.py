a5b98cec14eb7aa6ed997add620057a5caf7107d
izathakimi <izathakimiismayatim@gmail.com>
2025-04-03 06:42:33 +0000

    update

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 
+import torch
+print(torch.__version__)
+print("CUDA Available:", torch.cuda.is_available())
+
+
 from transformers import pipeline
 pipe = pipeline("automatic-speech-recognition", model="mesolitica/malaysian-whisper-large-v2")
